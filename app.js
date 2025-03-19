7f5b074f8255a9ca659909cd76a3d252308982d4
CristinaMtzR18 <cris.mtz.r18@gmail.com>
2025-03-18 21:46:25 -0600

    Agregando funciones de agregar, mostrar y sortear

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Lista de amigos
+let amigos = [];
+
+// Agrega un nuevo amigo si el campo no esta vacio
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+
+    if (nombreAmigo.trim() === "") {
+        alert("Ingresar un nombre");
+    } else {
+        amigos.push(nombreAmigo);
+        document.querySelector("#amigo").value = "";
+        mostrarListaAmigo();
+    }
+}
+
+// Muestra la lista de amigos en pantalla
+function mostrarListaAmigo() {
+    let listaAmigos = document.querySelector("#listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for (let index = 0; index < amigos.length; index++) {
+        const element = amigos[index];
+
+        let listaHTML = document.createElement("li");
+        listaHTML.textContent = element;
+        listaAmigos.appendChild(listaHTML);
+    }
+}
+
+// Realiza el sorteo si hay amigos en la lista
+function sortearAmigo() {
+    let cantidadAmigos = amigos.length;
+    if (cantidadAmigos === 0) {
+        alert("Se necesita nombres antes de sortear");
+    } else {
+        let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
+        let resultadoHTML = document.querySelector("#resultado");
+        resultadoHTML.innerHTML = amigos[indiceAmigo];
+    }
+}
